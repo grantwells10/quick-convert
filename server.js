bd644206d489ab81becd8d486d1d47280dcd0ac1
Grant Wells <47901670+grantwells10@users.noreply.github.com>
2024-05-13 19:44:30 -0400

    port update for render

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-const port = process.env.PORT || 3000; // PORT variable from Render
+const port = process.env.PORT || 4000; // PORT variable from Render
 
 app.use(express.json());
 
